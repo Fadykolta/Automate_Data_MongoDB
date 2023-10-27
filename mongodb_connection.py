cfd58648e41d088da485ec9ed83808e41830bcb8
test-env <test-env@Care-Ais-MacBook-Pro.local>
2023-10-27 11:57:15 -0400

    add database to test branch

--- mongodb_connection.py
+++ mongodb_connection.py
@@ -1,21 +1,14 @@
 import json
 import os
-
 from pymongo import MongoClient
 
-# Load the MongoDB connection configuration ### uncomment the following if you will add your MONGODB_URI in config.json file
-# with open('config.json', 'r') as file:
-#     config = json.load(file)
-
-# Load the data to be inserted
+# Load the data to be inserted from the JSON file
 with open('data.json', 'r') as file:
-    document = json.load(file)
+    data = json.load(file)
 
-# Connect to the MongoDB instance ### use config with MONGODB_URI if you will will add your MONGODB_URI in config.json file
-# client = MongoClient(config['MONGODB_URI'])
+# Connect to the MongoDB instance
 MONGODB_URI = os.environ.get('MONGODB_URI')
 client = MongoClient(MONGODB_URI)
-
 
 # Select (or create) the database 'mydatabase'
 db = client.surveyDB
@@ -23,10 +16,11 @@ db = client.surveyDB
 # Select (or create) the collection 'mycollection'
 collection = db.answers
 
-# Insert the document
-collection.insert_one(document)
+# Insert each document from the loaded data
+for document in data:
+    collection.insert_one(document)
 
-print("Document inserted successfully!")
+print("Documents inserted successfully!")
 
 # Close the connection
 client.close()
